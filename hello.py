14286f9e9b1bbfc127bd3f8afc346930366442ad
bakhtawar-10xe <bakhtawar.amjad@10xengineers.ai>
2023-02-09 11:59:56 +0500

    Updated hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
-def add(n1, n2):
-    return n1 + n2
+"""
+This module demonstrates the use of Pylint conventions.
+"""
+
+
+def add(n_1, n_2):
+    """
+    This Function demonstrates the use of Pylint conventions.
+    """
+    return n_1 + n_2
+
 
 print("hello world")
-X=20
-Y=10
-z = 3 + 3+ X * Y       
+X = 20
+Y = 10
+Z = 3 + 3 + X * Y
 print(X)
-print(add (X, Y))
+print(add(X, Y))
